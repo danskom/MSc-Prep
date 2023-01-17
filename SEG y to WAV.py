c3878afb9e6c7681f4e041a972be424197b40ce7
danskom <danskom@yahoo.com>
2023-01-17 21:42:59 +0000

    Add files via upload

--- SEG y to WAV.py
+++ SEG y to WAV.py
@@ -24,11 +24,20 @@ else:
 #convert list to an array
 
 import numpy as np
-npa = np.asarray(a, dtype=np.float32)
+
+#npa = np.asarray(a, dtype=np.float32) - original encoding not readable in wave module
+npa = np.asarray(a, dtype=np.int16)
+
+
+
 
 # convert array to a single line for mono output
 b = npa.ravel()
 
+print(b)
+
 from scipy.io.wavfile import write
-write("Seismic_to_WAV.wav",15902,b) #sample rate, used 44000 and a[0] and compared to length of trace in SeisSee, in my case and arrived at 15902.... was lazy.
+
+#write("Seismic_to_WAV.wav",15902,b) #sample rate, used 44000 and a[0] and compared to length of trace in SeisSee, in my case and arrived at 15902.... was lazy.
+write("Seismic_to_WAV3.wav",44100,b) #adjusted frequency 
 print("File written.")
