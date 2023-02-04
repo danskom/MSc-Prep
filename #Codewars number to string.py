7af99324af8bfeab174aa3b71d934087236e2f3b
danskom <danskom@yahoo.com>
2023-02-04 23:55:12 +0000

    Add files via upload

---
+++ #Codewars number to string.py
@@ -0,0 +1,14 @@
+#Codewars number to string
+
+
+list = ('1','2','3','4')
+
+def string_to_number(list):
+  
+    string = ''.join(list)
+    
+    integer = (int(string))
+
+    print(integer)
+
+string_to_number(list)
