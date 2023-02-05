4b94024339efedfff0b241e073df4b3f59ce38d7
danskom <danskom@yahoo.com>
2023-02-05 16:09:16 +0000

    Add files via upload

---
+++ #walrus operator and augmented operators.py
@@ -0,0 +1,31 @@
+#walrus operator and augmented operators
+
+#(walrus := True )
+#print (walrus)
+
+#(y := 3,8)
+
+#print(y)
+
+for i in range(-6,10):
+    
+    i = i+14
+    
+    print(i)
+
+    if i == 21:
+        print('this a for loop')
+
+    #i = i+1
+    
+    #while i < 9:
+    
+    #print(i+1)
+
+    #if i >= 10:
+    #print('you are a dick')
+
+
+    
+
+
